718b95035879f86aa1c85edbb2a98817d24057c1
georgewelly <cyberhylian@gmail.com>
2024-04-15 16:06:16 +0100

    added landing page path

--- backfill-UA.py
+++ backfill-UA.py
@@ -11,7 +11,7 @@ KEY_FILE_LOCATION = '../keys/gtm-w6kpsfd7-yjbhm-5808ebc38263.json'  # Path to yo
 VIEW_ID = '151196979'  # Your Google Analytics View ID
 BIGQUERY_PROJECT = 'gtm-w6kpsfd7-yjbhm'  # Your Google Cloud Project ID
 BIGQUERY_DATASET = 'ua_storage_test'  # BigQuery Dataset name where the data will be stored
-BIGQUERY_TABLE = 'ua-backfill-test'  # BigQuery Table name where the data will be stored
+BIGQUERY_TABLE = 'ua-backfill-3'  # BigQuery Table name where the data will be stored
 # Setting up the environment variable for Google Application Credentials
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = KEY_FILE_LOCATION
 
@@ -33,22 +33,25 @@ def get_report(analytics):
                     'dateRanges': [{'startDate': '2021-03-17', 'endDate': '2021-10-17'}],
                     # Metrics and dimensions are specified here
                     'metrics': [
-                        {'expression': 'ga:sessions'},
+                        # {'expression': 'ga:sessions'},
                         {'expression': 'ga:pageviews'},
-                        {'expression': 'ga:users'},
-                        {'expression': 'ga:newUsers'},
-                        {'expression': 'ga:bounceRate'},
-                        {'expression': 'ga:sessionDuration'},
-                        {'expression': 'ga:avgSessionDuration'},
-                        {'expression': 'ga:pageviewsPerSession'},
+                        # {'expression': 'ga:users'},
+                        # {'expression': 'ga:newUsers'},
+                        # {'expression': 'ga:bounceRate'},
+                        # {'expression': 'ga:sessionDuration'},
+                        # {'expression': 'ga:avgSessionDuration'},
+                        # {'expression': 'ga:pageviewsPerSession'},
                         # Add or remove metrics as per your requirements
                     ],
-                    'dimensions': [
+                    'dimensions': [ # 2 + date is the limit !!!
                         # {'name': 'ga:country'},
                         # {'name': 'ga:pageTitle'},
                         # {'name': 'ga:browser'},
-                        {'name': 'ga:channelGrouping'},
-                        {'name': 'ga:source'},
+                        # {'name': 'ga:pagePath'},
+                        # {'name': 'ga:source'},
+                        {'name':'ga:landingPagePath'},
+                        {'name': 'ga:campaign'},
+                        {'name': 'ga:date'},
                         #{'name': 'ga:pagePath'},
                         # {'name': 'ga:deviceCategory'},
                         # Add or remove dimensions as per your requirements
